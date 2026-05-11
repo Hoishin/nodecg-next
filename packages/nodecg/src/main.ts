6e7ec5f9d500b5f1b6520da159c3d5cba2bfd20f
Keiichiro Amemiya <git@hoishin.dev>
2026-05-11 22:16:21 +0200

    websocket server

--- packages/nodecg/src/main.ts
+++ packages/nodecg/src/main.ts
@@ -3,13 +3,45 @@ import { createServer } from "node:http";
 import {
 	HttpRouter,
 	HttpServer,
+	HttpServerRequest,
 	HttpServerResponse,
 } from "@effect/platform";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
-import { Layer } from "effect";
+import { Effect, Layer, Match, Schema } from "effect";
+
+const ClientMessage = Schema.Union(
+	Schema.TaggedStruct("subscribe", { topic: Schema.String }),
+	Schema.TaggedStruct("publish", {
+		topic: Schema.String,
+		value: Schema.Unknown,
+	}),
+	Schema.TaggedStruct("ping", {}),
+);
+type ClientMessage = typeof ClientMessage.Type;
+
+const decodeClientMessage = Schema.decode(Schema.parseJson(ClientMessage));
+
+const handleMessage = (msg: ClientMessage) =>
+	Match.value(msg).pipe(
+		Match.tag("subscribe", ({ topic }) => Effect.log(`sub: ${topic}`)),
+		Match.tag("publish", ({ topic }) => Effect.log(`pub: ${topic}`)),
+		Match.tag("ping", () => Effect.log("ping")),
+		Match.exhaustive,
+	);
+
+const wsHandler = Effect.gen(function* () {
+	const socket = yield* HttpServerRequest.upgrade;
+	yield* socket.runRaw((data) =>
+		typeof data === "string"
+			? decodeClientMessage(data).pipe(Effect.flatMap(handleMessage))
+			: Effect.void,
+	);
+	return HttpServerResponse.empty();
+});
 
 const router = HttpRouter.empty.pipe(
 	HttpRouter.get("/", HttpServerResponse.text("OK")),
+	HttpRouter.get("/ws", wsHandler),
 );
 
 HttpServer.serve(router).pipe(
